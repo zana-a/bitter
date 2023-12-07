24cd1798e3b53ff456064e653aa3b9c2ca89fcf1
zana-a <zana200@icloud.com>
2023-12-07 15:53:51 +0000

    Create Spring Boot application

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    java
+    application
+
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.zana"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+java {
+    toolchain {
+        sourceCompatibility = JavaVersion.VERSION_21
+        version = JavaVersion.VERSION_21
+    }
+}
+
+application {
+    mainClass = "com.zana.bitter.BitterApplication"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/zana/bitter/BitterApplication.java
@@ -0,0 +1,12 @@
+package com.zana.bitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitterApplication.class, args);
+    }
+}
